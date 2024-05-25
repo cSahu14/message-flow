bf5515a2dec9b9a0d1b914e1491e009f697692ec
cSahu14 <sahuchidananda1999@gmail.com>
2024-05-25 09:13:22 +0530

    add title

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import DnDFlow from './DndFlow/DnDFlow';
 import Navbar from './DndFlow/Navbar';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = "Message Flow"
+  }, [])
   return (
     <div style={{ width: "100%", height: "93vh" }}>
       <Navbar />
